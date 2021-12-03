e6f36b20aa791bdcf34a3baeac534d0c5bb532ef
PalaniGit <palanisamyremotejob@gmail.com>
2021-12-03 19:12:15 +0530

    Funny Learny 1) Fragment animation implementation added

--- app/src/main/java/com/local/funnylearny/ui/base/ActivityExtensions.kt
+++ app/src/main/java/com/local/funnylearny/ui/base/ActivityExtensions.kt
@@ -6,7 +6,7 @@ import com.local.funnylearny.R
 
 fun FragmentActivity.addFragment(container: Int, fragment: Fragment, tag: String? = null) {
     supportFragmentManager.beginTransaction().apply {
-        setCustomAnimations(R.animator.fade_in, 0, 0, R.animator.fade_out)
+        setCustomAnimations(R.animator.slide_up, 0, 0, R.animator.slide_down)
         add(container, fragment, tag).addToBackStack(tag).commit()
     }
 }
