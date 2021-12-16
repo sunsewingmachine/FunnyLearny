ab8f742d7adca01b8aa5c0bef6967e52add53497
PalaniGit <palanisamyremotejob@gmail.com>
2021-12-16 20:54:56 +0530

    True or False Question Fragment changes

--- app/src/main/java/com/local/funnylearny/ui/trueorfalsequestion/CardStackViewAdapter.kt
+++ app/src/main/java/com/local/funnylearny/ui/trueorfalsequestion/CardStackViewAdapter.kt
@@ -1,39 +1,98 @@
 package com.local.funnylearny.ui.trueorfalsequestion
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.local.funnylearny.databinding.TrueOrFalseQuestionListItemBinding
 
-class CardStackViewAdapter(private var trueOrFalseQuestion : ArrayList<TrueOrFalseQuestion>) : RecyclerView.Adapter<CardStackViewAdapter.ViewHolder>() {
+class CardStackViewAdapter(
+    var trueOrFalseQuestion: ArrayList<TrueOrFalseQuestion>,
+    private val onTrueOrFalseQuestionAdapterListener: OnTrueOrFalseQuestionAdapterListener
+) : RecyclerView.Adapter<CardStackViewAdapter.ViewHolder>() {
+
+    private var isRefreshing = false
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        return ViewHolder(TrueOrFalseQuestionListItemBinding.inflate( LayoutInflater.from(parent.context),
-            parent,
-            false))
+        return ViewHolder(
+            TrueOrFalseQuestionListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val trueOrFalseQuestion = trueOrFalseQuestion[position]
-        holder.trueOrFalseQuestionText.text = trueOrFalseQuestion.question
-        holder.reasonText.text = trueOrFalseQuestion.reason
-
-
+        holder.apply {
+            trueOrFalseQuestionText.text = trueOrFalseQuestion.question
+            reasonText.text = trueOrFalseQuestion.reason
+            if (isRefreshing) {
+                //Change into container visibility handling
+                trueButton.visibility = View.GONE
+                falseButton.visibility = View.GONE
+                reasonLayout.visibility = View.VISIBLE
+                reasonText.text = trueOrFalseQuestion.reason
+                isRefreshing = false
+            } else {
+                trueButton.visibility = View.VISIBLE
+                falseButton.visibility = View.VISIBLE
+                reasonLayout.visibility = View.GONE
+            }
+        }
     }
 
     override fun getItemCount(): Int = trueOrFalseQuestion.size
 
 
-   inner class ViewHolder(binding : TrueOrFalseQuestionListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-      var trueOrFalseQuestionText : TextView = binding.trueOrFalseQuestionText
-       var reasonText : TextView = binding.reasonText
-       var nextButton : MaterialButton  = binding.nextButton
-       var trueButton : MaterialButton = binding.trueButton
-       var falseButton : MaterialButton = binding.falseButton
+    inner class ViewHolder(binding: TrueOrFalseQuestionListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        var trueOrFalseQuestionText: TextView = binding.trueOrFalseQuestionText
+        var reasonText: TextView = binding.reasonText
+        var trueButton: MaterialButton = binding.trueButton
+        var falseButton: MaterialButton = binding.falseButton
+        var reasonLayout : LinearLayout = binding.reasonLayout
+
+        init {
+            trueButton.setOnClickListener {
+                onTrueButtonClicked(absoluteAdapterPosition)
+            }
+            falseButton.setOnClickListener {
+                onFalseButtonClicked(absoluteAdapterPosition)
+            }
+        }
     }
+
+    private fun onTrueButtonClicked(position: Int) {
+        if (trueOrFalseQuestion[position].answer) {
+            onTrueOrFalseQuestionAdapterListener.onCorrectAnswerClicked()
+        } else {
+            isRefreshing = true
+            notifyItemChanged(position)
+            onTrueOrFalseQuestionAdapterListener.onWrongAnswerClicked(position)
+        }
+    }
+
+    private fun onFalseButtonClicked(position: Int) {
+        if (!trueOrFalseQuestion[position].answer) {
+            onTrueOrFalseQuestionAdapterListener.onCorrectAnswerClicked()
+        } else {
+            isRefreshing = true
+            notifyItemChanged(position)
+            onTrueOrFalseQuestionAdapterListener.onWrongAnswerClicked(position)
+        }
+    }
+
+    interface OnTrueOrFalseQuestionAdapterListener {
+        fun onCorrectAnswerClicked()
+        fun onWrongAnswerClicked(position: Int)
+    }
+
 }
--- app/src/main/java/com/local/funnylearny/ui/trueorfalsequestion/TrueOrFalseQuestionFragment.kt
+++ app/src/main/java/com/local/funnylearny/ui/trueorfalsequestion/TrueOrFalseQuestionFragment.kt
@@ -9,16 +9,19 @@ import android.view.ViewGroup
 import com.local.funnylearny.R
 import com.local.funnylearny.ui.base.FragmentInteractionListener
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.StackFrom
 import kotlinx.android.synthetic.main.fragment_true_or_false_question.*
 import java.lang.IllegalArgumentException
 
 class TrueOrFalseQuestionFragment : Fragment() {
 
-    private var trueOrFalseQuestionFragmentInteractionListener : TrueOrFalseQuestionFragmentInteractionListener? = null
+    private var trueOrFalseQuestionFragmentInteractionListener: TrueOrFalseQuestionFragmentInteractionListener? =
+        null
+    private lateinit var cardStackLayoutManager: CardStackLayoutManager
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if(context is TrueOrFalseQuestionFragmentInteractionListener){
+        if (context is TrueOrFalseQuestionFragmentInteractionListener) {
             trueOrFalseQuestionFragmentInteractionListener = context
         } else {
             throw IllegalArgumentException("TrueOrFalseQuestionFragmentInteractionListener is not implemented in respective activity")
@@ -38,22 +41,74 @@ class TrueOrFalseQuestionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolBar()
-        trueOrFalseQuestionList.add(TrueOrFalseQuestion(
-            "100","First ODI (Cricket) in India was played in Ahemadabad","True","Ahmedabad had tha honour of hosting the first One Day International match played in India on November 25, 1981. India, captained by Sunil Gavaskar"
-        ))
-        trueOrFalseQuestionList.add(TrueOrFalseQuestion("101"," The first captain of the first ODI Indian team was Sunil Gavaskar","False","The first captain of the first ODI Indian team was Ajit Wadekar"))
-        trueOrFalseQuestionList.add(TrueOrFalseQuestion("102","The Ramayana was written by Tulsidas","False","The Ramayana was written by Valmiki"))
-        trueOrFalseQuestionList.add(TrueOrFalseQuestion("103","Shakespeare wrote 37 plays","True","Between about 1590 and 1613, Shakespeare wrote at least 37 plays"))
-        trueOrFalseQuestionList.add(TrueOrFalseQuestion("104","Sound waves travel faster than light","False","Light travels faster than sound waves"))
+        trueOrFalseQuestionList.add(
+            TrueOrFalseQuestion(
+                "100",
+                "First ODI (Cricket) in India was played in Ahemadabad",
+                true,
+                "Ahmedabad had tha honour of hosting the first One Day International match played in India on November 25, 1981. India, captained by Sunil Gavaskar"
+            )
+        )
+        trueOrFalseQuestionList.add(
+            TrueOrFalseQuestion(
+                "101",
+                " The first captain of the first ODI Indian team was Sunil Gavaskar",
+                false,
+                "The first captain of the first ODI Indian team was Ajit Wadekar"
+            )
+        )
+        trueOrFalseQuestionList.add(
+            TrueOrFalseQuestion(
+                "102",
+                "The Ramayana was written by Tulsidas",
+                false,
+                "The Ramayana was written by Valmiki"
+            )
+        )
+        trueOrFalseQuestionList.add(
+            TrueOrFalseQuestion(
+                "103",
+                "Shakespeare wrote 37 plays",
+                true,
+                "Between about 1590 and 1613, Shakespeare wrote at least 37 plays"
+            )
+        )
+        trueOrFalseQuestionList.add(
+            TrueOrFalseQuestion(
+                "104",
+                "Sound waves travel faster than light",
+                false,
+                "Light travels faster than sound waves"
+            )
+        )
         adapterAttachment()
+
+        nextButton.setOnClickListener {
+            trueOrFalseQuestionCardStackView.swipe()
+            nextButton.visibility = View.GONE
+        }
     }
 
-    private fun adapterAttachment(){
-        trueOrFalseQuestionCardStackView.layoutManager = CardStackLayoutManager()
-        trueOrFalseQuestionCardStackView.adapter = CardStackViewAdapter(trueOrFalseQuestion = trueOrFalseQuestion)
+    private fun adapterAttachment() {
+        cardStackLayoutManager = CardStackLayoutManager(context)
+        trueOrFalseQuestionCardStackView.layoutManager = cardStackLayoutManager
+        cardStackLayoutManager.setStackFrom(StackFrom.Top)
+        cardStackLayoutManager.setVisibleCount(4)
+        trueOrFalseQuestionCardStackView.adapter = CardStackViewAdapter(trueOrFalseQuestionList,object : CardStackViewAdapter.OnTrueOrFalseQuestionAdapterListener {
+            override fun onCorrectAnswerClicked() {
+                trueOrFalseQuestionCardStackView.swipe()
+            }
+            override fun onWrongAnswerClicked(position : Int) {
+                if(position != trueOrFalseQuestionList.size-1) {
+                    nextButton.visibility = View.VISIBLE
+                } else {
+
+                }
+            }
+        })
     }
 
-    private fun initToolBar(){
+    private fun initToolBar() {
         trueOrFalseQuestionToolBar.setNavigationOnClickListener {
             trueOrFalseQuestionFragmentInteractionListener?.onNavigationIconClickedListener()
         }
